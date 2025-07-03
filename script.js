b52d9221957040e594351aae2c908213c7677df3
Rayliff <raihanaliff25@gmail.com>
2025-07-03 22:36:04 +0800

    update dikit

--- script.js
+++ script.js
@@ -3,17 +3,30 @@ function startCelebration() {
   document.getElementById('main-content').classList.remove('hidden');
   document.getElementById('bg-music').play();
 
-  const typed = new Typed('#typed', {
-    strings: [
-      'Selamat ulang tahun mamah tercinta!',
-      'Semoga panjang umur, sehat selalu, dan bahagia selalu.',
-      'Aku sangat sayang mamah ❤️'
-    ],
-    typeSpeed: 40,
-    backSpeed: 20,
-    loop: true
+   // Ketik kalimat 1
+  const typed1 = new Typed('#typed-1', {
+    strings: ['Selamat ulang tahun mamah tercinta!'],
+    typeSpeed: 100,
+    showCursor: false,
+    onComplete: () => {
+      // Ketik kalimat 2 setelah selesai
+      const typed2 = new Typed('#typed-2', {
+        strings: ['Semoga panjang umur, sehat selalu, dan bahagia selalu.'],
+        typeSpeed: 100,
+        showCursor: false,
+        onComplete: () => {
+          // Ketik kalimat 3 setelah selesai
+          const typed3 = new Typed('#typed-3', {
+            strings: ['The one and only Queen in the family❤️'],
+            typeSpeed: 100,
+            showCursor: false
+          });
+        }
+      });
+    }
   });
 
+  // Confetti tetap jalan
   setInterval(() => {
     confetti({
       particleCount: 3,
